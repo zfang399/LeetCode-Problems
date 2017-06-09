9a869e9e2cc3782a42045bd2a280625de2bff563
Zhaoyuan Fang <zfang@nd.edu>
2017-06-09 09:58:39 -0400

    Add files via upload

---
+++ 283.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int now=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]){
+                nums[now]=nums[i];
+                now++;
+            }
+        }
+        for(int i=now;i<nums.size();i++) nums[i]=0;
+    }
+};
