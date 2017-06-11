f7ae87ad8df3917a0035a160e7d3f80570291df8
Zhaoyuan Fang <zfang@nd.edu>
2017-06-11 11:36:37 -0400

    Add files via upload

---
+++ 242.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int count[26]={0};
+        for(int i=0;i<s.length();i++){
+            count[s[i]-'a']++;
+        }
+        for(int i=0;i<t.length();i++){
+            if(count[t[i]-'a']==0) return false;
+            count[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(count[i]){
+               return false;
+            }
+        }
+        return true;
+    }
+};
