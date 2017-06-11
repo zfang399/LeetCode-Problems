689b94026c7b97d46992bb2de7396a9de83abae2
Zhaoyuan Fang <zfang@nd.edu>
2017-06-10 23:19:47 -0400

    Add files via upload

---
+++ 387.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int count[26]={0};
+        for(int i=0;i<s.length();i++){
+            count[s[i]-'a']++;
+        }
+        for(int i=0;i<s.length();i++){
+            if(count[s[i]-'a']==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
