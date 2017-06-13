497f79578bd8be93e0a54163e158bdfa562d1ae6
Zhaoyuan Fang <zfang@nd.edu>
2017-06-13 09:14:07 -0400

    Add files via upload

---
+++ 21.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(!l1) return l2;
+        if(!l2) return l1;
+        if(!l1 && !l2) return NULL;
+        ListNode* head;
+        if(l1->val<l2->val){
+            head=l1;
+            l1=l1->next;
+        }else{
+            head=l2;
+            l2=l2->next;
+        }
+        ListNode* cur=head;
+        while(l1 && l2){
+            if(l1->val < l2->val){
+                cur->next=l1;
+                l1=l1->next;
+            }else{
+                cur->next=l2;
+                l2=l2->next;
+            }
+            cur=cur->next;
+        }
+        while(l1){
+            cur->next=l1;
+            l1=l1->next;
+            cur=cur->next;
+        }
+        while(l2){
+            cur->next=l2;
+            l2=l2->next;
+            cur=cur->next;
+        }
+        return head;
+    }
+};
---
+++ 263.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isUgly(int num) {
+        if(num<=0) return false;
+        if(num==1) return true;
+        while(num%5==0) num/=5;
+        while(num%3==0) num/=3;
+        while(num%2==0) num/=2;
+        if(num==1) return true;
+        return false;
+    }
+};
