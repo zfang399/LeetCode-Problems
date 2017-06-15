593aee19df68f86502e5a2a9f276b0e394532587
Zhaoyuan Fang <zfang@nd.edu>
2017-06-15 15:43:15 -0400

    Add files via upload

---
+++ 438.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> ans;
+        if(s.length()<p.length()) return ans;
+        vector<int> pc(26),sc(26);
+        for(int i=0;i<p.length();i++) pc[p[i]-'a']++;
+        for(int i=0;i<p.length();i++) sc[s[i]-'a']++;
+        if(pc==sc) ans.push_back(0);
+        int pl=p.length();
+        for(int i=pl;i<s.length();i++){
+            sc[s[i]-'a']++;
+            sc[s[i-pl]-'a']--;
+            if(pc==sc) ans.push_back(i-pl+1);
+        }
+        return ans;
+    }
+};
