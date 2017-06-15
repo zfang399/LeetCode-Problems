98d45d0c5f0e86cd1a1432b26b45361a96dfb06b
Zhaoyuan Fang <zfang@nd.edu>
2017-06-15 15:56:35 -0400

    Add files via upload

---
+++ 205.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.length()!=t.length()) return false;
+        unordered_map <char,char> refs,reft;
+        for(int i=0;i<s.length();i++){
+            if(!refs[s[i]] && !reft[t[i]]){
+                refs[s[i]]=t[i];
+                reft[t[i]]=s[i];
+            }else if (refs[s[i]]!=t[i] || reft[t[i]]!=s[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
