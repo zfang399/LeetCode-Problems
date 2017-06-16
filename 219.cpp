c475d2e70cb460c84cd5e9c27ee773dfbf053f00
Zhaoyuan Fang <zfang@nd.edu>
2017-06-16 16:05:59 -0400

    Add files via upload

---
+++ 219.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        map <int,int> index;
+        for(int i=0;i<nums.size();i++){
+            if(index[nums[i]]==0){
+                index[nums[i]]=i+1;
+            }else if(i-index[nums[i]]+1<=k){
+                return true;
+            }else{
+                index[nums[i]]=i+1;
+            }
+        }
+        return false;
+    }
+};
