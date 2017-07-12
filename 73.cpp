cf63156cd300ef59b62c000a574bbea22609e3e5
Zhaoyuan Fang <zfang@nd.edu>
2017-07-11 22:09:17 -0400

    Set Matrix Zeroes

---
+++ 73.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int fc=1;
+        for(int i=0;i<matrix.size();i++){
+            if(matrix[i][0]==0) fc=0;
+            for(int j=1;j<matrix[i].size();j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        for (int i=matrix.size()-1; i>=0;i--) {
+            for (int j=matrix[i].size()-1;j>=1;j--)
+            if (matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+            if (fc==0) matrix[i][0]=0;
+        }
+    }
+};
