2a524565b5084b64a703bb11c1fcdf83892153d9
Zhaoyuan Fang <zfang@nd.edu>
2017-10-10 17:39:19 -0400

    Odd Even Linked List

---
+++ 328.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head==NULL) return NULL;
+        ListNode *odd=head,*tmp=head->next,*even=head->next;
+        while(even && even->next){
+            odd->next=odd->next->next;
+            even->next=even->next->next;
+            odd=odd->next;
+            even=even->next;
+        }
+        odd->next=tmp;
+        return head;
+    }
+};
