894ac55c17118bb1e645c6ad032ebc8738d8ed2a
Zhaoyuan Fang <zfang@nd.edu>
2017-10-28 23:56:39 -0400

    Majority Element

--- 169.cpp
+++ 169.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int x=nums.size()/2;
+        int x=nums.size()/2; 
         map <int,int> count;
         for(int i=0;i<nums.size();i++){
             count[nums[i]]++;
