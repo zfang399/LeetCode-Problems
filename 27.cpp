d7c989cddeb3d9382d0ea50ef8faee65ea08962c
Zhaoyuan Fang <zfang@nd.edu>
2017-11-28 21:35:19 -0500

    Remove Element

--- 27.cpp
+++ 27.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int f=0,r=nums.size()-1;
-        while(f<=r){
-            if(nums[f]==val){
-                nums[f]=nums[r];
-                nums[r]=val;
+        int f = 0, r = nums.size() - 1;
+        while(f <= r){
+            if(nums[f] == val){
+                nums[f] = nums[r];
+                nums[r] = val;
                 r--;
             }else{
                 f++;
             }
         }
-        return r+1;
+        return r + 1;
     }
 };
