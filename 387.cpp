66a4ee3b32b079fa59adefc71fb9788fc88b6d39
Zhaoyuan Fang <zfang@nd.edu>
2017-12-15 00:16:00 -0500

    First Unique Character in a String

--- 387.cpp
+++ 387.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int firstUniqChar(string s) {
         int count[26]={0};
-        for(int i=0;i<s.length();i++){
+        for(int i = 0; i < s.length(); i++){
             count[s[i]-'a']++;
         }
-        for(int i=0;i<s.length();i++){
+        for(int i = 0; i < s.length(); i++){
             if(count[s[i]-'a']==1){
                 return i;
             }
@@ -13,3 +13,4 @@ public:
         return -1;
     }
 };
+ 
