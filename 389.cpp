78abd60f5c032a6ebb5864f7e9828edb62f7ea9b
Zhaoyuan Fang <zfang@nd.edu>
2017-12-15 00:16:47 -0500

    Find the Difference

--- 389.cpp
+++ 389.cpp
@@ -3,15 +3,15 @@ public:
     char findTheDifference(string s, string t) {
         char ans;
         map<char,int> count;
-        for(int i=0;i<t.length();i++){
+        for(int i = 0; i < t.length(); i++){
             count[t[i]]++;
         }
-        for(int i=0;i<s.length();i++){
+        for(int i = 0; i < s.length(); i++){
             count[s[i]]--;
         }
-        for(int i=0;i<t.length();i++){
+        for(int i = 0; i < t.length(); i++){
             if(count[t[i]]!=0){
-                ans=t[i];
+                ans = t[i];
             }
         }
         return ans;
