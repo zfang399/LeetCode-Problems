a2107d210d3b13f208ce56a05ac4bc4ec7aa922e
Zhaoyuan Fang <zfang@nd.edu>
2017-12-16 02:02:04 -0500

    Fizz Buzz

--- 412.cpp
+++ 412.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     vector<string> fizzBuzz(int n) {
         vector<string> ans;
-        for(int i=1;i<=n;i++){
-            if(i%3==0 && i%5==0){
+        for(int i = 1; i <= n; i++){
+            if(i%3 == 0 && i%5 == 0){
                 ans.push_back("FizzBuzz");
             }
-            else if(i%3==0){
+            else if(i%3 == 0){
                 ans.push_back("Fizz");
             }
-            else if(i%5==0){
+            else if(i%5 == 0){
                 ans.push_back("Buzz");
             }
             else{
