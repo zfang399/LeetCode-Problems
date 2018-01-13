42cfc31ccd44b5ce42fe004cb6d1872a8e6d7be4
Zhaoyuan Fang <zfang@nd.edu>
2018-01-13 18:58:29 +0800

    Path Sum III

--- 437.cpp
+++ 437.cpp
@@ -10,13 +10,13 @@
 class Solution {
 public:
     int pathSum(TreeNode* root, int sum) {
-        if(root==NULL) return 0;
-        return pathSum(root->left,sum)+pathSum(root->right,sum)+prefixSum(root,0,sum);
+        if(root == NULL) return 0;
+        return pathSum(root->left, sum) + pathSum(root->right, sum) + prefixSum(root, 0, sum);
     }
 private:
     int prefixSum(TreeNode* root, int pre, int& sum){
-        if(root==NULL) return 0;
-        int now=pre+root->val;
-        return (now==sum)+prefixSum(root->left,now,sum)+prefixSum(root->right,now,sum);
+        if(root == NULL) return 0;
+        int now = pre + root->val;
+        return (now == sum) + prefixSum(root->left, now, sum)+prefixSum(root->right, now, sum);
     }
 };
