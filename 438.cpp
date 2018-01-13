660d71590cbad40b73f80ee667fc9dd6e0ef2210
Zhaoyuan Fang <zfang@nd.edu>
2018-01-13 18:59:21 +0800

    Find All Anagrams in a String

--- 438.cpp
+++ 438.cpp
@@ -4,14 +4,14 @@ public:
         vector<int> ans;
         if(s.length()<p.length()) return ans;
         vector<int> pc(26),sc(26);
-        for(int i=0;i<p.length();i++) pc[p[i]-'a']++;
-        for(int i=0;i<p.length();i++) sc[s[i]-'a']++;
-        if(pc==sc) ans.push_back(0);
-        int pl=p.length();
-        for(int i=pl;i<s.length();i++){
+        for(int i=0; i<p.length(); i++) pc[p[i]-'a']++;
+        for(int i=0; i<p.length(); i++) sc[s[i]-'a']++;
+        if(pc == sc) ans.push_back(0);
+        int pl = p.length();
+        for(int i = pl; i < s.length(); i++){
             sc[s[i]-'a']++;
             sc[s[i-pl]-'a']--;
-            if(pc==sc) ans.push_back(i-pl+1);
+            if(pc == sc) ans.push_back(i-pl+1);
         }
         return ans;
     }
