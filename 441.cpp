c800463168580080d57ba7f2f2dbca6b03e69334
Zhaoyuan Fang <zfang@nd.edu>
2018-01-13 19:00:15 +0800

    Arranging Coins

--- 441.cpp
+++ 441.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        long long f=1,r=n,mid;
-        while(f<=r){
-            mid=(f+r)/2;
-            if(mid*(mid+1)/2<=n){
-                f=mid+1;
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        long long f = 1, r = n, mid;
+        while(f <= r){
+            mid = (f + r)/2;
+            if(mid*(mid+1)/2 <= n){
+                f = mid + 1;
             }else{
-                r=mid-1;
+                r = mid - 1;
             }
         }
         return f-1;
