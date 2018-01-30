555d5bda7b13e4b7eed22abf8e9718eb14ee35e0
Zhaoyuan Fang <zfang@nd.edu>
2018-01-29 23:57:42 -0500

    Hamming Distance

--- 461.cpp
+++ 461.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int hammingDistance(int x, int y) {
         int tx,ty,ans=0;
-        while((x/2!=0 || x%2!=0) || (y/2!=0 || y%2!=0)){
-            tx=x%2;
-            ty=y%2;
-            x=x/2;
-            y=y/2;
-            if(tx!=ty){
+        while((x / 2 != 0 || x % 2 != 0) || (y / 2 != 0 || y % 2 != 0)){
+            tx = x % 2;
+            ty = y % 2;
+            x = x / 2;
+            y = y / 2;
+            if(tx != ty){
                 ans++;
             }
         }
