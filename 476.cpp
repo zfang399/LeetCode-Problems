ed2cfb793a0316fcf4e25365de3bbad0c86e3842
Zhaoyuan Fang <zfang@nd.edu>
2018-02-03 22:58:36 -0500

    Number Complement

--- 476.cpp
+++ 476.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int findComplement(int num) {
-        long long ans=1;
-        while(ans<=num){
-            ans*=2;
+        long long ans = 1;
+        while(ans <= num){
+            ans *= 2;
         }
-        return ans-num-1;
+        return ans - num - 1;
     }
 };
