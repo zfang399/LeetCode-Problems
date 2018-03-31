823dd20a672ebeb2d359954fe951fab90951acf9
Zhaoyuan Fang <zfang@nd.edu>
2018-03-31 17:11:58 -0400

    Max Consecutive Ones

--- 485.cpp
+++ 485.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        int maxr=0,ins=0;
-        for(int i=0;i<nums.size();i++){
+        int maxr = 0, ins = 0;
+        for(int i = 0; i < nums.size(); i++){
             if(nums[i]){
                 ins++;
             }else{
-                if(ins>maxr) maxr=ins;
-                ins=0;
+                if(ins > maxr) maxr = ins;
+                ins = 0;
             }
         }
-        if(ins>maxr) maxr=ins;
+        if(ins > maxr) maxr = ins;
         return maxr;
     }
 };
