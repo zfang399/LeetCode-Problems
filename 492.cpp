e52afb9ab17a82ed7074d4fe5d3257f5032f970e
Zhaoyuan Fang <zfang@nd.edu>
2018-03-31 17:12:37 -0400

    Construct the Rectangle

--- 492.cpp
+++ 492.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> constructRectangle(int area) {
-        int mid=sqrt(area);
+        int mid = sqrt(area);
         vector<int> ans;
-        for(int i=mid;i>=1;i--){
-            if(area%i==0){
-                if(i>area/i){
+        for(int i = mid; i >= 1; i--){
+            if(area % i == 0){
+                if(i > area / i){
                     ans.push_back(i);
-                    ans.push_back(area/i);
+                    ans.push_back(area / i);
                 }else{
-                    ans.push_back(area/i);
+                    ans.push_back(area / i);
                     ans.push_back(i);
                 }
                 return ans;
